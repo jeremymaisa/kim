a1e2d89e9a7c585469598e4a8231434f8a6053ce
jeremy <maisajeremy331@gmail.com>
2026-02-17 15:20:33 +0800

    try submit button if its working

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,3 +1,11 @@
+import { app, analytics } from "./firebase.js";
 // Register user
 
-const email = 
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+const submit = document.getElementById('submit').value;
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  alert(5);
+});
